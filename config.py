63fae9777a35119b3b7e401aafcf0eeee9763310
CristiCh <cristi.chertes@yahoo.com>
2025-07-13 18:20:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-OPENAI_API_KEY = "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"#os.getenv("OPENAI_API_KEY")
-SENDER_EMAIL = "cristi.chertes@gmail.com" #os.getenv("SENDER_EMAIL")
-SENDER_PASSWORD = "zxtp nzjw dsvi lila" #os.getenv("SENDER_PASSWORD")
-RECIPIENT_EMAIL = "cristi.chertes@gmail.com" #os.getenv("RECIPIENT_EMAIL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
